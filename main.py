0e6f4b3e8dc327f3f4d40ac82cd39e988a488e4f
Lillac <30539721+lillac@users.noreply.github.com>
2017-11-09 21:00:59 +1300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
-from random import randrange
-print('Random Number Generator')
-l = int(input('Enter min number'))
-h = int(input('Enter max number'))
-gu = (randrange(l, h))
-st = input("Would you like to try guess the random number? y/n")
-if st == 'n':
-  print("Okay, here is your random number:")
-  print(gu)
-elif st == 'y':
-  gs = input("Guess..")
-  if gs == gu:
-    print("Correct!")
-  elif gs != gu:
-    print("Incorrect..")
-    print("The answer is:")
-    print(gu)
+print("Random number generator")
+from random import randrange 
+def ran(one, two):
+  return randrange(one, two)
+  
